f9b1f525be20d46d148dcb41a221282b4e3b871d
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2022-01-28 20:07:14 +0530

    first commit

---
+++ Basics_Challenge.py
@@ -0,0 +1,74 @@
+#1 Ask for the user's first name and display the output message
+
+#firstname = input("Please enter your name:")
+#print("Hello",firstname)
+
+#2 Ask for the username and surname and then display the output
+
+#firstname = input("Enter your firstname: ")
+#surname = input("Enter your surname: ")
+#print(firstname,surname)
+
+#3 Write a code that will display "What do you call a bear with no teeth?" and on the next line display "A gummy bear!". Write a single line of code for that
+
+#print("What do you call a bear without teeth? \n A gummy bear!")
+
+
+#4 Ask the user to enter two numbers. Add them together and display the answer as The total is [answer]
+
+#num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# answer = num1 + num2
+# print("The answer is", answer)
+
+#5 Ask the user to enter three numbers. Add together the two numbers and then multiply this total by the third. Display answer  is [answer]
+
+# num1 = int(input("Enter the first number : "))
+# num2 = int(input("Enter the second number : "))
+# num3 = int(input("Enter the third number : "))
+# answer = ( num1 + num2 ) * num3
+# print("The answer is : ",answer)
+
+#6 Ask how many slices of pizza the user started with and ask how many slices they have eaten . Work out how many slices they have left and display the aser in a user friendly format
+
+# total_slices = int(input("Please!, Enter the pizza slices you start with :"))
+# slices = int(input("Please!, Enter the pizza slices you have eaten :"))
+# remaining = total_slices - slices
+# print("Only", remaining , "slices have left")
+
+#7 Ask the user for their name and their age. Add 1 to their age and display the output [Name] next birthday you will be [new age] 
+
+# name = input("Enter your name : ")
+# age = int(input("Enter your age : "))
+# new_age = age + 1
+# print(name, "next birthday you will be ", new_age)
+
+#8 Ask for the total price of the bill, then ask how many diners there are. Divide the total bill by the number of diners and show how much each person must pay.
+
+# total = int(input("Please! Enter your total bill :"))
+# dinner = int(input("Enter the number of peoples having dinner : "))
+# print("The total bill amount is : ",total)
+# each_person_pay = total/dinner
+# print(each_person_pay,"have to paid by each person ")
+
+#9 Write a program that will ask for a number of days and then will show how many hours, minutes and seconds are in that number of days
+
+# days = int(input("Enter the number of days :"))
+# hours = days*24
+# minutes = hours*60
+# seconds = minutes*60
+# print("This day have", hours ,"hours",  minutes, "minutes", seconds, "seconds")
+
+#10 There are 2,204 pounds in a kilogram. Ask the user to enter a weight in kilogram and convert it in pounds
+
+# weight = int(input("Enter the weight(in kilogram) : "))
+# pound = 2204*weight
+# print("This weight have", pound, "pounds")
+
+#11 Task the user to enter a number over 100 and then enter a number under 10 and tell them how many times the smaller number goes into the larger number in a user friendy format
+
+# num1 = int(input("Enter a number over 100 :"))
+# num2 = int(input("Enter a number over 10 :"))
+# answer = num1 // num2
+# print(num2, "goes into", num1, "times")
+
---
+++ If_Else.py
@@ -0,0 +1,56 @@
+#12 Ask for two numbers. If the first one is larger than the second, display the second number first and then the first number, otherwise show the first number first and then the second
+
+# a = int(input("Enter the value of a: "))
+# b = int(input("Enter the value of b: "))
+# if a>b:
+    # print("The value of a is " , b, "and the value of b is",a)
+# else:
+    # print("The value of a is ", a, "and the value of b is", b)
+
+
+
+#13 Ask the user to enter a number that is undr 20. If they enter a number that is 20 or more, display the message "Too high", otherwise display "Thank you"
+
+# num = int(input("Enter a number under 20 :"))
+# if num>=20:
+    # print("Too High")
+# else:
+    # print("Thank You !")
+
+
+#14 Ask the user to enter a number between 10 and 20 . If they enter a number within this range, display the message "Thank You", otherwise display the message "Incorrect answer".
+
+# num = int(input("Enter a number between 10 and 20 :"))
+# if num>=10 and num<=20:
+    # print("Thank You !")
+# else:
+    # print("Incorrect answer!")
+
+
+
+#15 Ask the user to enter their favourite color. If they enter "red ", "RED" or "Red" display the message "I like red too", otherwise display the message "I don't like [color], I prefer red".
+
+
+# color = input("Enter your favourite color : ")
+# if color == "RED" or color == "Red" or color == "red":
+    # print("I like red too")
+# else:
+    # print("I don't like", color,", I prefer red")
+
+
+#16 Ask the user if it is raining and convert their ansewer to lower case so it doesnt't matter what case they type it in. If they answer "Yes" ask if it is windy. If they answer "yes" to this second question, display the answer "It is too windy for an umberella", otherwise display the message "Take an umberella", If they did not answer yes to the first question, display the answer "Enjoy your day"
+
+raining = input("Is it raining today :")
+raining = str.lower(raining)
+ #for convert the answer into lower case
+if raining == "yes":
+    windy = input("Is it windy? ")
+    windy = str.lower(windy)
+    if windy == "yes":
+        print("It is too windy for an umberella")
+    else:
+        print("Take an umberella")
+else:
+    print("Enjoy your day")
+
+
