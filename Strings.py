b6be641cb2d3bed2c1d18ace4d42249a46afdeb6
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2022-01-28 23:17:17 +0530

    almost complete the challenge of strings

---
+++ Strings.py
@@ -0,0 +1,48 @@
+#20 Ask the user to enter their first name and then display the length of their name.
+
+# name = input("Enter their first name : ")
+# length = len(name) #len is used for counting the characters
+# print(length)
+
+#21 Ask the user to enter their first name and then ask them to enter their surname. Join them together with a space between and display the name and the length of whole name.
+
+# name = input("Enter your first name :")
+# surname = input("Enter your surname :")
+# full_name = name + " " + surname
+# length = len(full_name)
+# print(full_name, "and the length is : ", length)
+
+#22 Ask the user to enter their first name and surname in lower case. Change the case to title case and join them together.Display the finished result.
+
+# name = input("Enter your name : ")
+# name = name.title()
+# surname = input("Enter your surname : ")
+# surname = surname.title()
+# full_name = name + " " + surname
+# print(full_name)
+
+#23 Ask the user to type in the first line of a nursery rhyme and display the length of the string. Ask for a starting number and an ending number and then display just that section of the text (remember Python starts counting from 0 and not 1).
+
+# line = input("Enter a nurserry line :")
+# length = len(line)
+# start = int(input("Enter the starting number :"))
+# end = int(input("Enter the ending number :"))
+# part = (line[start:end])
+# print(part)
+
+#24 Ask the user to type in any word and display it in upper case.
+
+# word = input("Enter a word :")
+# word = word.upper()
+# print(word)
+
+#25 
+
+
+
+
+
+
+
+
+
