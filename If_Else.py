656673e944ba73e8027036873dd61c3bdd694fdd
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2022-01-28 20:24:59 +0530

    Challenge series if else almost complete

--- If_Else.py
+++ If_Else.py
@@ -43,14 +43,36 @@
 raining = input("Is it raining today :")
 raining = str.lower(raining)
  #for convert the answer into lower case
-if raining == "yes":
-    windy = input("Is it windy? ")
-    windy = str.lower(windy)
-    if windy == "yes":
-        print("It is too windy for an umberella")
-    else:
-        print("Take an umberella")
+# if raining == "yes":
+#     windy = input("Is it windy? ")
+#     windy = str.lower(windy)
+#     if windy == "yes":
+#         print("It is too windy for an umberella")
+#     else:
+#         print("Take an umberella")
+# else:
+#     print("Enjoy your day")
+
+#17 Ask the user's age. If they are 18 or over,display the message "You can vote", if they are aged 17, display the message "You can learn to drive", if they are 16, display the message "You can buy a lottery ticket", if they are under 16, display the message "You can go Trick-or-Treating"
+
+# age = int(input("Enter your age :"))
+# if age >= 18:
+#     print("You can vote")
+#     if age == 17:
+#         print("You can learn to drive")
+#         if age == 16:
+#             print("You can buy a lottery ticket")
+# else:
+#     print("You can go Trick-or-Treating")
+
+#18 Ask the user to enter a number, If it is under 10, display the message "Too low", if their number is between 10 and 20, display "Correct", otherwise display "Too high".
+
+num = int(input("Enter a number :"))
+if num <= 10:
+    print("Too low")
+    if num >= 10 and num <= 20:
+        print("Correct")
 else:
-    print("Enjoy your day")
+    print("Too high")
 
 
